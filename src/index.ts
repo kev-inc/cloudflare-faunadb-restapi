e2e367fcfb47221905c5648ee656a505d7d6bb73
Kevin <kevinchanxy@gmail.com>
2022-06-20 10:02:59 +0000

    added fauna

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,17 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-}
-
 import {Router} from 'itty-router'
+import { withParams } from 'itty-router-extras'
 import { corsHelper } from './cors-helper'
+import faunadb from 'faunadb'
 
 const router = Router();
+const q = faunadb.query;
+
+const client = new faunadb.Client({
+  secret: '',
+});
 
 router.options('*', corsHelper)
+
 router.get('/', () => {
   return new Response("Hello GET!")
 })
@@ -39,6 +26,43 @@ router.put('/', () => {
 
 router.delete('/', () => {
   return new Response("Hello DELETE")
+})
+
+router.get('/todos', async () => {
+  try {
+    const data = await client.query(
+      q.Map(
+        q.Paginate(q.Documents(q.Collection('todos'))),
+        q.Lambda("X", q.Get(q.Var("X")))
+      )
+    )
+    return new Response(JSON.stringify(data, null, 2))
+  } catch {
+    return new Response("Error")
+  }
+})
+
+router.get('/todos/:id', withParams, async ({params}) => {
+  try {
+    const id = params ? params.id : ''
+    const data = await client.query(
+      q.Get(q.Ref(q.Collection('todos'), id))
+    )
+    return new Response(JSON.stringify(data, null, 2))
+  } catch {
+    return new Response("Error")
+  }
+})
+
+router.post('/todos', async () => {
+  try {
+    const createTodo = await client.query(
+      q.Create(q.Collection('todos'), {data: {name: "Hello Fauna"}})
+    )
+    return new Response(JSON.stringify(createTodo, null, 2))
+  } catch {
+    return new Response("Error")
+  }
 })
 
 // 404 for everything else
